e3bf1799297dfd8754f49816697a355b5db7a0eb
Sansan91 <zarnor91@gmail.com>
2013-12-12 08:03:04 +0100

    Börjat fixa på SellStreet

---
+++ MonopolyBoard/SellStreet.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MonopolyBoard
+{
+    partial class SellStreet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clbStreets = new System.Windows.Forms.CheckedListBox();
+            this.btnSell = new System.Windows.Forms.Button();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clbStreets
+            // 
+            this.clbStreets.CheckOnClick = true;
+            this.clbStreets.FormattingEnabled = true;
+            this.clbStreets.Location = new System.Drawing.Point(12, 12);
+            this.clbStreets.Name = "clbStreets";
+            this.clbStreets.Size = new System.Drawing.Size(195, 154);
+            this.clbStreets.TabIndex = 0;
+            this.clbStreets.SelectedIndexChanged += new System.EventHandler(this.clbStreets_SelectedIndexChanged);
+            // 
+            // btnSell
+            // 
+            this.btnSell.Location = new System.Drawing.Point(216, 37);
+            this.btnSell.Name = "btnSell";
+            this.btnSell.Size = new System.Drawing.Size(75, 23);
+            this.btnSell.TabIndex = 1;
+            this.btnSell.Text = "Sälj gata";
+            this.btnSell.UseVisualStyleBackColor = true;
+            this.btnSell.Click += new System.EventHandler(this.btnSell_Click);
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Location = new System.Drawing.Point(234, 103);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(35, 13);
+            this.lbPrice.TabIndex = 2;
+            this.lbPrice.Text = "label1";
+            // 
+            // SellStreet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 176);
+            this.Controls.Add(this.lbPrice);
+            this.Controls.Add(this.btnSell);
+            this.Controls.Add(this.clbStreets);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "SellStreet";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SellStreet";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.SellStreet_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clbStreets;
+        private System.Windows.Forms.Button btnSell;
+        private System.Windows.Forms.Label lbPrice;
+    }
+}
---
+++ MonopolyBoard/SellStreet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonopolyBoard
+{
+    public partial class SellStreet : Form
+    {
+        public frmMonopoly board;
+        public SellStreet()
+        {
+            InitializeComponent();
+        }
+
+        private void SellStreet_Load(object sender, EventArgs e)
+        {
+            int player = board.activePlayer;
+            foreach (Square square in board.SquaresArray)
+            {
+                if (square.GetType() == typeof(Street) && ((Street)square).GetOwner() == player)
+                {
+                    clbStreets.Items.Add(((Street)square).GetName());
+                }
+                else if (square.GetType() == typeof(Station) && ((Station)square).GetOwner() == player)
+                {
+                    clbStreets.Items.Add(((Station)square).GetName());
+                }
+                else if (square.GetType() == typeof(PowerStation) && ((PowerStation)square).GetOwner() == player)
+                {
+                    clbStreets.Items.Add(((PowerStation)square).GetName());
+                }
+            }
+        }
+
+        private void btnSell_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < board.SquaresArray.Length; i++)
+            {
+                Square sellSquare = board.SquaresArray[i];
+                foreach (object square in clbStreets.CheckedItems)
+                {
+                    if (square.ToString() == board.SquaresArray[i].GetName())
+                    {
+                        if (square.GetType() == typeof(Street))
+                        {
+                            ((Street)sellSquare).ChangeOwner(5);
+                            board.Player[board.activePlayer].AddMoney(((Street)sellSquare).GetSellPrice());
+                        }
+                        else if (square.GetType() == typeof(Station))
+                        {
+                            ((Station)sellSquare).ChangeOwner(5);
+                            board.Player[board.activePlayer].AddMoney(((Station)sellSquare).GetSellPrice());
+                        }
+                        else if (square.GetType() == typeof(PowerStation))
+                        {
+                            ((PowerStation)sellSquare).ChangeOwner(5);
+                            board.Player[board.activePlayer].AddMoney(((PowerStation)sellSquare).GetSellPrice());
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void clbStreets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
